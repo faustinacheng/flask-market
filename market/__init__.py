830a2e6b550ea393dcc87d272f4aee59a3230fe9
Faustina Cheng <chengfaustina@gmail.com>
2021-07-31 03:04:21 +0800

    Add redirect to login and login user after registration

--- market/__init__.py
+++ market/__init__.py
@@ -9,5 +9,7 @@ app.config["SECRET_KEY"] = "5116d890cb07d8bc52709d20"
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, login_required
 
 
 @app.route("/")
@@ -13,6 +13,7 @@ def home_page():
 
 
 @app.route("/market")
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template("market.html", items=items)
@@ -26,6 +27,10 @@ def register_page():
                               password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
+
+        login_user(user_to_create)
+        flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category="success")
+
         return redirect(url_for("market_page"))
     if form.errors:
         for err_msg in form.errors.values():
