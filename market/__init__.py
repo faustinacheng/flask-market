a2ccbde09f6a1511737913397325b653ccc95069
Faustina Cheng <chengfaustina@gmail.com>
2021-08-02 12:02:52 +0800

    Change db URI

--- market/__init__.py
+++ market/__init__.py
@@ -1,11 +1,12 @@
 from os import environ
+import re
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = environ.get('DATABASE_URL') or "sqlite:///market.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DATABASE_URL?sslmode=require") or "sqlite:///market.db"
 app.config["SECRET_KEY"] = "5116d890cb07d8bc52709d20"
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
--- market/db_create.py
+++
@@ -1,2 +0,0 @@
-from market import db
-db.create_all()
