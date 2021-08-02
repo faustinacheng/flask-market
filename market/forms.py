a97ec77103633a6bd2989fd813d671be3fa35c02
Faustina Cheng <chengfaustina@gmail.com>
2021-08-03 07:19:28 +0800

    Customize flash messages

--- market/forms.py
+++ market/forms.py
@@ -5,10 +5,10 @@ from market.models import User
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label="User Name: ", validators=[Length(min=2, max=30), DataRequired()])
-    email_address = StringField(label="Email Address: ", validators=[Email(), DataRequired()])
-    password1 = PasswordField(label="Password: ", validators=[Length(min=6), DataRequired()])
-    password2 = PasswordField(label="Confirm Password: ", validators=[EqualTo("password1"), DataRequired()])
+    username = StringField(label="User Name: ", validators=[Length(min=2, max=30, message="Username must be between 2 to 30 characters long."), DataRequired()])
+    email_address = StringField(label="Email Address: ", validators=[Email(message="Email address is invalid."), DataRequired()])
+    password1 = PasswordField(label="Password: ", validators=[Length(min=6, message="Password must be at least 6 characters long."), DataRequired()])
+    password2 = PasswordField(label="Confirm Password: ", validators=[EqualTo(fieldname="password1", message="Passwords do not match."), DataRequired()])
     submit = SubmitField(label="Create Account")
 
     def validate_username(self, username_to_check):
--- market/routes.py
+++ market/routes.py
@@ -67,7 +67,7 @@ def register_page():
         return redirect(url_for("market_page"))
     if form.errors:
         for err_msg in form.errors.values():
-            flash(f"There was an error with creating a user: {err_msg[0]}", category="danger")
+            flash(f"{err_msg[0]}", category="danger")
     return render_template("register.html", form=form)
 
 
