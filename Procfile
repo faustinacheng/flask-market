f35de45db45cf91719fccc699520aa1f49435d94
Faustina Cheng <chengfaustina@gmail.com>
2021-08-02 11:29:33 +0800

    Prepare for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn market:app
--- market/__init__.py
+++ market/__init__.py
@@ -1,10 +1,11 @@
+from os import environ
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = environ.get('DATABASE_URL') or "sqlite:///market.db"
 app.config["SECRET_KEY"] = "5116d890cb07d8bc52709d20"
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
